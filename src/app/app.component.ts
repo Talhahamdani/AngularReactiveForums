edb47cd53822dcaa2d9f3750e252122eec9cd241
Syed Talha Hamdani <syedtalhahamdani99@gmail.com>
2024-01-08 16:32:46 +0500

    New Changes updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import {FormControl, FormGroup, FormBuilder, Validators, FormArray} from "@angular/forms";
+
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,67 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'myform';
+  title = 'My Reactive Form1';
+  title2 = 'My Reactive Form2';
+
+  loginForm:FormGroup;
+  loginForm2:FormGroup;
+  constructor(private fb:FormBuilder)
+  {
+
+  this.loginForm=fb.group({
+    name:new FormControl('',[Validators.required]),
+    email:new FormControl('',[Validators.required]),
+    phoneno:new FormControl('',[Validators.required]),
+    company:new FormControl('',[Validators.required]),
+    address:new FormArray([
+      new FormControl(null),
+      new FormControl(null)
+    ])
+  })
+    this.loginForm2=fb.group({
+      name:new FormControl('',[Validators.required]),
+      email:new FormControl('',[Validators.required]),
+      phoneno:new FormControl('',[Validators.required]),
+      company:new FormControl('',[Validators.required]),
+    })
+
+
+}
+
+
+  formsubmitted: boolean = false;
+  isformSubmitted:boolean = false;
+
+  onsubmit(value:any)
+  {
+    if (this.loginForm.valid)
+    {
+      this.formsubmitted = true;
+      this.isformSubmitted = true;
+      console.log(this.loginForm.value);
+    }
+    else
+    {
+      this.loginForm.markAllAsTouched();
+      console.log('All fields are required. Please fill in all the fields.');
+    }
+  }
+
+  get name()
+  {
+    return this.loginForm.get("name")
+  }
+  get email()
+  {
+    return this.loginForm.get("email")
+  }
+  get phoneno()
+  {
+    return this.loginForm.get("phoneno")
+  }
+  get company()
+  {
+    return this.loginForm.get("company")
+  }
 }
